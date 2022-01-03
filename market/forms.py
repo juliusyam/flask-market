a6a1c55d3606cf03db9c2f53d02da2d4c4c588c1
juliusyam <juliusyam@users.noreply.github.com>
2022-01-03 18:12:01 +0000

    provide username and email unique check

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import Length, EqualTo, Email, DataRequired
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please try a different one.')
+
+    def validate_email_address(self, email_address_to_check):
+        user = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if user:
+            raise ValidationError('Email address already taken. Please try a different one.')
+
     username = StringField(label='Username', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
