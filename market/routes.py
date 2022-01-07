86dae8571d85cf28027da33673f99672c1372134
juliusyam <juliusyam@users.noreply.github.com>
2022-01-07 13:58:25 +0000

    provided logged in user context and redirects

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app, db
-from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -11,6 +12,7 @@ def home_page():
 
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -28,6 +30,9 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
 
+        login_user(user_to_create)
+        flash(f'Account created successfully. You\'re now logged in as {user_to_create.username}', category='success')
+
         return redirect(url_for('market_page'))
 
     if form.errors != {}:
@@ -40,4 +45,29 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as {attempted_user.username}', category='success')
+
+            return redirect(url_for('market_page'))
+
+        else:
+            flash('Username and password do not match! Please try again.', category='danger')
+
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+
+    logout_user()
+
+    flash(f'You\'ve successfully logged out', category='info')
+
+    return redirect(url_for('home_page'))
